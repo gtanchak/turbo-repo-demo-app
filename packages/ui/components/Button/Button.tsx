64dcd9431d9f3dcde19903a11bf44ce83d0df5b5
gtanchak <ghtanchak77@gmail.com>
2023-12-24 18:54:54 +0530

    fix: fixed component library

--- packages/ui/components/Button/Button.tsx
+++ packages/ui/components/Button/Button.tsx
@@ -2,8 +2,8 @@ import React, { FC } from "react";
 import { Button as MuiButton } from "@mui/material";
 import { MuiButtonProps } from "./types";
 
-const Button: FC<MuiButtonProps> = ({ children }) => {
-  return <MuiButton>{children}</MuiButton>;
+const Button: FC<MuiButtonProps> = ({ children, ...rest }) => {
+  return <MuiButton {...rest}>{children}</MuiButton>;
 };
 
 export default Button;
