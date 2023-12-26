7d0043551c68064a06c496f4d822a63df41aa50f
gtanchak <ghtanchak77@gmail.com>
2023-12-26 21:59:21 +0530

    remove unused code

--- apps/user/src/App.tsx
+++ apps/user/src/App.tsx
@@ -1,40 +1,9 @@
 import { Button } from "@repo/ui";
-import { useEffect, useState } from "react";
-
-function Child() {
-  console.log("%c Child: render", "color: mediumspringgreen");
-  const [count, setCount] = useState(() => {
-    console.log("%c Child: useState(): 0", "color: mediumturquoise");
-    return 0;
-  });
-
-  useEffect(() => {
-    console.log("%c App: useEffect", "color: tomato");
-    return () => {
-      console.log("%c App: cleanup useEffect", "color:orange");
-    };
-  });
-
-  return (
-    <div>
-      <Button onClick={() => setCount((prev) => prev + 1)}>{count}</Button>child{" "}
-      {count}
-    </div>
-  );
-}
 
 function App() {
-  console.log("%c, App:render start", "color: lightcoral");
-  const [childCount, setChildCount] = useState(() => {
-    console.log("%c App: useState(): false", "color: mediumturquoise");
-    return false;
-  });
-  console.log("%c, App:render end", "color: pink");
-
   return (
     <>
       <Button>button</Button>
-      <Child />
     </>
   );
 }
